ec71d9628647c7dffaa0d271da5727225579dafe
luohongyin <lhythu@gmail.com>
2025-03-06 04:21:57 +0000

    assert attn mask

--- csrc/flash_attn/src/mask.h
+++ csrc/flash_attn/src/mask.h
@@ -160,6 +160,7 @@ struct Mask {
                         auto mask_val = attn_mask.data()[col_idx];
                         assert(attn_mask[col_idx] == 0 || attn_mask[col_idx] == 1);
                         assert(mask_val != 0 && mask_val != 1);
+                        assert(mask_val == 1);
                         
                         // TORCH_CHECK(mask_val == 0 || mask_val == 1);
                         
@@ -198,6 +199,7 @@ struct Mask {
                                 const int mask_val = attn_mask.data()[col_idx];
                                 assert(attn_mask[col_idx] == 0 || attn_mask[col_idx] == 1);
                                 assert(mask_val != 0 && mask_val != 1);
+                                assert(mask_val == 1);
                                 // TORCH_CHECK(mask_val == 0 || mask_val == 1);
 
                                 if constexpr (Has_alibi) {
